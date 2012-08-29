90630aa1dadfc6129b023287a03acb838f87b0a2
Travis Crawford <travis@twitter.com>
2012-08-29 16:31:04 -0700

    Use Hive "ql.metadata" classes for Table and Partition whenever possible, except when communicating with the HiveMetaStore, or serializing. The "ql.metadata" versions of these classes provide additional business logic beyond what's stored in the metastore.

--- src/java/org/apache/hcatalog/mapreduce/FileOutputFormatContainer.java
+++ src/java/org/apache/hcatalog/mapreduce/FileOutputFormatContainer.java
@@ -27,7 +27,7 @@ import org.apache.hadoop.hive.metastore.HiveMetaStoreClient;
 import org.apache.hadoop.hive.metastore.api.FieldSchema;
 import org.apache.hadoop.hive.metastore.api.MetaException;
 import org.apache.hadoop.hive.metastore.api.NoSuchObjectException;
-import org.apache.hadoop.hive.metastore.api.Table;
+import org.apache.hadoop.hive.ql.metadata.Table;
 import org.apache.hadoop.hive.serde2.SerDe;
 import org.apache.hadoop.io.NullWritable;
 import org.apache.hadoop.io.Writable;
@@ -190,7 +190,7 @@ class FileOutputFormatContainer extends OutputFormatContainer {
                     table, outputInfo.getPartitionValues());
             // non-partitioned table
 
-            Path tablePath = new Path(table.getSd().getLocation());
+            Path tablePath = new Path(table.getTTable().getSd().getLocation());
             FileSystem fs = tablePath.getFileSystem(context.getConfiguration());
 
             if ( fs.exists(tablePath) ) {
--- src/java/org/apache/hcatalog/mapreduce/HCatTableInfo.java
+++ src/java/org/apache/hcatalog/mapreduce/HCatTableInfo.java
@@ -22,7 +22,7 @@ import java.io.IOException;
 import java.io.Serializable;
 
 import org.apache.hadoop.hive.metastore.MetaStoreUtils;
-import org.apache.hadoop.hive.metastore.api.Table;
+import org.apache.hadoop.hive.ql.metadata.Table;
 import org.apache.hcatalog.common.HCatUtil;
 import org.apache.hcatalog.data.schema.HCatSchema;
 
@@ -118,7 +118,7 @@ public class HCatTableInfo implements Serializable {
   }
 
   public String getTableLocation() {
-      return table.getSd().getLocation();
+      return table.getTTable().getSd().getLocation();
   }
 
   /**
@@ -137,10 +137,9 @@ public class HCatTableInfo implements Serializable {
    * @throws IOException
    */
   static HCatTableInfo valueOf(Table table) throws IOException {
-    HCatSchema dataColumns = 
-        HCatUtil.extractSchemaFromStorageDescriptor(table.getSd());
+    HCatSchema dataColumns = HCatUtil.extractSchema(table);
     StorerInfo storerInfo = 
-        InternalUtil.extractStorerInfo(table.getSd(), table.getParameters());
+        InternalUtil.extractStorerInfo(table.getTTable().getSd(), table.getParameters());
     HCatSchema partitionColumns = HCatUtil.getPartitionColumns(table);
     return new HCatTableInfo(table.getDbName(),
                              table.getTableName(),
