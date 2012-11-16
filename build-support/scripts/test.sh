c1dee913d5012da763639bdb78cd366fe17e3565
Travis Crawford <travis@twitter.com>
2012-11-16 09:27:28 -0800

    Update hadoop23 so webhcat builds with 20 as it does not support 23 at this time.

---
+++ build-support/scripts/test.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+function run_cmd() {
+  echo "Running command: ${cmd}"
+  ${cmd}
+  if [ $? != 0 ]; then
+    echo "Failed!"
+    exit 1
+  fi
+}
+
+cmd='ant clean package -Dmvn.profile=hadoop20'
+run_cmd
+
+cmd='ant -v clean package -Dmvn.profile=hadoop23'
+run_cmd
+
